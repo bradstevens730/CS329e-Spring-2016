cd0dd5f221bc52900b53ab4b35d2775661e8d631
Nick <nickolas.grisham@gmail.com>
2016-04-20 18:40:11 -0500

    writes to file, info from GEAR austin

--- BeautyTest.py
+++ BeautyTest.py
@@ -1,17 +1,42 @@
 import requests
 from bs4 import BeautifulSoup
+outFile=open('mainFile','w')
 
-gearLight = requests.get("http://www.hdgear.tv/LIGHTING")
-soupGearLight = BeautifulSoup(gearLight.content)
+outFile.close()
+outFile=open('mainFile','a')
 
-pricelist = []
-gLightProdPrice = soupGearLight.find_all("div",{"class":"priceBox"})
-for item in gLightProdPrice:
-	stripper=item.text
-	stripper.strip("$")
-	pricelist.append(stripper)
-gLightProdName = soupGearLight.find_all("div",{"class":"listText"})
-
-for item in gLightProdName:
-	print (item.text, ":",pricelist.pop(0), "\n\n")
+def gearLightInfo():
 	
+
+	gearLight = requests.get("http://www.hdgear.tv/LIGHTING")
+	soupGearLight = BeautifulSoup(gearLight.content,"html.parser")
+
+	pricelist = []
+	gLightProdPrice = soupGearLight.find_all("div",{"class":"priceBox"})
+	for item in gLightProdPrice:
+		stripper=item.text
+		stripper.strip("$")
+		pricelist.append(stripper)
+	gLightProdName = soupGearLight.find_all("div",{"class":"listText"})
+
+	for item in gLightProdName:
+		texto=item.text
+		priceo=pricelist.pop(0)
+		s=texto + ":" + priceo + "\n\n"
+		outFile.write(s)
+def amgLightInfo():
+	'''amgLight = requests.get("http://austinmoviegear.com/")
+	soupAMGLight = BeautifulSoup(amgLight.content,"html.parser")
+	
+	amgLightProdPrice = soupAMGLight.find_all("img",{"class":"grpelem"})
+	for item in amgLightProdPrice:
+		print(item.text)
+		'''
+		
+	
+def main():
+	gearLightInfo()
+	#amgLightInfo()
+
+main()
+outFile.close()
